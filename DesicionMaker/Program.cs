fd3c2ffa3e923bbc246d3e5fc200baec89b20d4e
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-05-21 10:20:58 +0200

    Initial commit

---
+++ DesicionMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesicionMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
