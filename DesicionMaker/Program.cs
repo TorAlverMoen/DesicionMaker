6216ac56847ff74c81a0f564dbcb328013763f6d
Tor Alver Moen <tor.alver.moen@gmail.com>
2024-05-22 13:37:38 +0200

    Choose pairs of inputs from the top of the list

--- DesicionMaker/Program.cs
+++ DesicionMaker/Program.cs
@@ -37,8 +37,44 @@ namespace DesicionMaker
 
             while (options.Count > 1)
             {
+                List<string> nextRound = new List<string>();    // A new list with the chosen options
 
+                for (int i = 0; i < options.Count; i += 2)
+                {
+                    if (i + 1 < options.Count)
+                    {
+                        string option1 = options[i];
+                        string option2 = options[i + 1];
+
+                        Console.WriteLine($"Choose between:\n1.{option1}\n2.{option2}");
+                        Console.Write("Enter 1 or 2: ");
+                        string choice = Console.ReadLine();
+
+                        if (choice == "1")
+                        {
+                            nextRound.Add(option1);
+                        }
+                        else if (choice == "2")
+                        {
+                            nextRound.Add(option2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid choice. Please enter 1 or 2");
+                            i -= 2;     // Move the counter back one pair
+                        }
+                    }
+                    else
+                    {
+                        // If there is one choice left at bottom of the list, automatically add that to the next round
+                        nextRound.Add(options[i]);
+                    }
+                }
+
+                options = nextRound;
             }
+
+            Console.WriteLine($"\nBest option: {options[0]}");
         }
 
         static void Main(string[] args)
